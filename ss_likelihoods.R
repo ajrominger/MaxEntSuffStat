4d6ad250fc0322acbd40cdce4b62c80612a9fb51
ajrominger <ajrominger@gmail.com>
2016-09-20 15:06:15 -0700

    got loglik working for ssnti

--- ss_likelihoods.R
+++ ss_likelihoods.R
@@ -1,4 +1,4 @@
-bci <- read.csv('~/Research/datasets/stri/all/BCIS/BCIS.csv')
+bci <- read.csv('~/Research/datasets/stri/all/BCIS/BCIS.csv', as.is = TRUE)
 bci <- bci[bci$year == max(bci$year), ]
 bciSpLevel <- aggregate(bci[, c('count', 'dbh')], list(spp = bci$spp), sum)
 
@@ -35,3 +35,28 @@ ssmeLogLik <- function(n, M) {
 
 ssntLogLik(bciSpLevel$count, bciSpLevel$dbh)
 ssmeLogLik(bciSpLevel$count, bciSpLevel$dbh)
+
+
+
+## likelihood function for SSNTI
+ssntiLogLik <- function(spp, n, m) {
+    S <- length(unique(spp))
+    nbar <- sum(n) / S
+    nu <- uniroot(nuSSNT, interval = c(.Machine$double.eps, 10), 
+                  nbar = nbar, tol = .Machine$double.eps)$root
+    
+    dat <- split(data.frame(n = n, m = m), spp)
+    
+    lp <- sapply(dat, function(d) {
+        m0 <- sum(d$m) / nbar
+        n <- sum(d$n)
+        
+        n*log(1 - nu) - log(n) - n*log(m0) - sum(d$m/m0)
+    })
+    
+    return(S * log(1/log(1/nu)) + sum(lp))
+}
+
+ssntiLogLik(bci$spp, bci$count, bci$dbh)
+
+
