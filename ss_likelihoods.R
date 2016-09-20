dcf80ae50e20ba4385e3b453dee1255008a903b1
ajrominger <ajrominger@gmail.com>
2016-09-20 14:47:30 -0700

    got likelihoods for species aggretate theories working

--- ss_likelihoods.R
+++ ss_likelihoods.R
@@ -1,5 +1,6 @@
 bci <- read.csv('~/Research/datasets/stri/all/BCIS/BCIS.csv')
 bci <- bci[bci$year == max(bci$year), ]
+bciSpLevel <- aggregate(bci[, c('count', 'dbh')], list(spp = bci$spp), sum)
 
 ## solution for nu under SSNT
 nuSSNT <- function(nu, nbar) {
@@ -16,6 +17,21 @@ ssntLogLik <- function(n, M) {
     nu <- uniroot(nuSSNT, interval = c(.Machine$double.eps, 10), 
                   nbar = nbar, tol = .Machine$double.eps)$root
     
-    S * log(1 / (m0*log(1/nu))) * sum(log() - M/m0)
+    S * log(1 / (m0*log(1/nu))) + 
+        sum(n*log(1-nu) - lfactorial(n) + (n-1)*log(M/m0) - M/m0)
 }
 
+## likelihood function for SSME
+ssmeLogLik <- function(n, M) {
+    S <- length(n)
+    nbar <- mean(n)
+    Mbar <- mean(M)
+    la1 <- log(nbar / (nbar - 1))
+    la2 <- 1/Mbar
+    
+    S * log(la2 * (exp(la1) - 1)) - sum(la1*n + la2*M)
+}
+
+
+ssntLogLik(bciSpLevel$count, bciSpLevel$dbh)
+ssmeLogLik(bciSpLevel$count, bciSpLevel$dbh)
